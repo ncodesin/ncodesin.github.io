72228796fd1f42f8b8f8501cc8479f14808d7004
ncodesin <nswk1230@gmail.com>
2023-01-20 16:14:36 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
-
+import $ from 'jquery';
+import axios from 'axios';
+import { Link, Outlet, Route, Router, Routes, useLocation, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import Main from './components/main/Main';
+import List from './components/list/List';
+import Profile from './components/profile/Profile';
+import { AnimatePresence, motion } from 'framer-motion';
+import Experience from './components/experience/Experience';
 function App() {
+
+  const location = useLocation();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+    <>
+      <AnimatePresence>
+        <Routes location={location} key={location.pathname}>
+          <Route exact path="/" element={
+            <>
+              <Main />
+            </>
+          } />
+          <Route path="/list" element={
+            <List />
+          }
+          />
+          <Route path="/profile" element={
+            <Profile />
+          }
+          />
+          <Route path="/experience" element={
+            <Experience />
+          } />
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
